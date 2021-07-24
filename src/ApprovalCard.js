c61d63291b8643ac8e160016040fb88824f2de20
Bharath425 <bharatyalla4@gmail.com>
2021-07-24 23:09:27 +0530

    1st change

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -21,6 +21,7 @@ const  ApprovalCard= (props)=>{
                             <div class="ui two buttons">
                                 <div class="ui basic green button">Approve</div>
                                 <div class="ui basic red button">Decline</div>
+                                <div></div>
                             </div>
                         </div>
                 </div>
